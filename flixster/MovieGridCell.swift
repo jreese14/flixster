0938cb5f70d6f47f3de7c3153f38286b7be698b1
jreese14 <jreesestige@gmail.com>
2021-02-18 21:13:39 -0800

    Part 2 of Flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jahnae Reese 2 on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
